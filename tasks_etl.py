da0eab98dee59c7384118e4c440c19623f3d3f15
lserra <laercio.serra@gmail.com>
2018-12-28 10:47:31 -0200

    moving the code from github.py to task_etl.py

--- tasks_etl.py
+++ tasks_etl.py
@@ -3,17 +3,85 @@
 
 
 """
-This task should be run in a daily cronjob to look for new tips and update
-stats (number of likes and retweets) on existing tweets.
+API GitHub
+Search for all the repositories in the GitHub and storing all the data into a
+SQLite database.
+
+This task should be run daily through a cronjob to check for new tips and
+update the stats (number of likes and retweets) on existing tweets.
 The tables are recreated daily.
 """
-from tasks.import_tweets import get_hashtag, get_tweet
-from tips.db import (
-    truncate_tables, get_hashtags, add_hashtags, get_tips, add_tips
-    )
+import sys
+import requests
+import sqlite3 as db
+
+
+def insert_sql_template(item):
+    description = str(item.get('description')).replace('"', '')
+
+    return """INSERT INTO repositories ('id','name','full_name','description', \
+    'homepage','git_url','ssh_url','language','private','archived') \
+    VALUES ("{}","{}","{}","{}","{}","{}","{}","{}","{}","{}")
+    """.format(
+        item.get('id'), item.get('name'), item.get('full_name'),
+        description, item.get('homepage'), item.get('git_url'),
+        item.get('ssh_url'), item.get('language'), item.get('private'),
+        item.get('archived')
+        )
+
+
+def insert_repos(items):
+    con = None
+
+    try:
+        con = db.connect(
+            '/home/lserra/PycharmProjects/api_github/data/github.db'
+            )
+
+        cur = con.cursor()
+
+        for item in items:
+            sql = insert_sql_template(item)
+            cur.execute(sql)
+
+        con.commit()
+
+        print(">> Total rows inserted: {}\n".format(len(items)))
+
+    except db.DatabaseError as e:
+        print(">> Error %s: " % e.args[0])
+        sys.exit(1)
+
+    finally:
+        if con is not None:
+            con.close()
+
+
+def get_repos(name_repo):
+    api_url_base = 'https://api.github.com/'
+    headers = {
+        'Content-Type': 'application/json',
+        'User-Agent': 'Python Student',
+        'Accept': 'application/vnd.github.v3+json'
+        }
+    api_url = '{}search/repositories?q={}+ \
+    language:py&sort=stars&order=desc'.format(api_url_base, name_repo)
+
+    r = requests.get(api_url, headers=headers)
+
+    if r.status_code == 200:
+        print("STATUS: Connection OK!\n")
+    else:
+        print("STATUS: Connection NOK. Error: {}\n".format(r.text))
+
+    return r.json()
 
 
 if __name__ == '__main__':
-    truncate_tables()
-    get_tweet()
-    get_hashtag()
+    repo_list = get_repos('airflow')
+    items = repo_list['items']
+
+    if items is not None:
+        insert_repos(items)
+    else:
+        print('>> No Repo List Found\n')
