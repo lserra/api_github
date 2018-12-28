be22214cc1337ee560d4b1a8af57d65536ca2b27
lserra <laercio.serra@gmail.com>
2018-12-28 17:12:13 -0200

    testing all the templates

--- github.py
+++ github.py
@@ -1,1 +1,27 @@
+#!/usr/bin/python3
 # -*- coding: utf-8 -*-
+
+
+import os
+# from tips.db import get_hashtags, get_tips
+from bottle import (
+    default_app, route, run, request, static_file, view, template
+    )
+
+
+@route('/static/<filename:path>')
+def send_static(filename):
+    return static_file(filename, root='static')
+
+
+@route('/')
+@route('/dashboard')
+# @view('dashboard')
+def dashboard():
+    return template('dashboard')
+
+
+if os.environ.get('APP_LOCATION') == 'heroku':
+    run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
+else:
+    run(host='localhost', port=8080, debug=True, reloader=True)
